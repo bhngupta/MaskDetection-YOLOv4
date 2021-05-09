946a404e3e12782d74d82d89bcf649113a560eac
Bhanu Gupta <bhanu.mbvg@gmail.com>
2021-05-09 21:17:04 +0530

    feat(add): custom darknet files

---
+++ scripts/yolov4_config.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 16 20:41:08 2020
+
+@author: Quang Nguyen
+"""
+
+""" 
+Generating custom cfg/yolov4_custom_train.cfg
+and cfg/yolov4_custom_test.cfg
+
+"""
+
+classes=2
+max_batches=6000
+batch=64
+subdivisions=16
+width=416
+height=416
+channels=3
+momentum=0.949
+decay=0.0005
+learning_rate=0.001
+steps= (4800,5400)
+scales=(0.1,0.1)
